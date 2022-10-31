b6fb348b014fa4ca618a4a6f1f7c3e15dd077701
helton <heltoloco@hotmail.com>
2022-10-31 18:03:00 -0300

    feat(bank): apply get balance method

---
+++ accounts/checkingAccount.go
@@ -0,0 +1,46 @@
+package accounts
+
+import (
+	"bank/clients"
+)
+
+type CheckingAccount struct {
+	Holder        clients.Holder
+	AgencyNumber  int
+	AccountNumber int
+	balance       float64
+}
+
+func (c *CheckingAccount) Withdraw(withdrawValue float64) string {
+	enabled := withdrawValue <= c.balance && withdrawValue > 0
+
+	if enabled {
+		c.balance -= withdrawValue
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *CheckingAccount) Deposit(depositValue float64) (string, float64) {
+	if depositValue > 0 {
+		c.balance += depositValue
+		return "Depósito realizado com sucesso", c.balance
+	} else {
+		return "Depósito não realizado, contate a gerência para mais infomações", c.balance
+	}
+}
+
+func (c *CheckingAccount) Transfer(transferValue float64, destinationAccount *CheckingAccount) bool {
+	if transferValue < c.balance && transferValue > 0 {
+		c.balance -= transferValue
+		destinationAccount.Deposit(transferValue)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *CheckingAccount) GetBalance() float64 {
+	return c.balance
+}
--- main.go
+++ main.go
@@ -1,53 +1,13 @@
 package main
 
-import "fmt"
-
-type CheckingAccount struct {
-	holder        string
-	agencyNumber  int
-	accountNumber int
-	balance       float64
-}
+import (
+	"bank/accounts"
+	"fmt"
+)
 
 func main() {
-	account001 := CheckingAccount{holder: "Helton", balance: 500}
-	account002 := CheckingAccount{holder: "Mark", balance: 200}
+	account001 := accounts.CheckingAccount{}
+	account001.Deposit(100)
 
-	fmt.Println(account001)
-	fmt.Println(account002)
-	status := account001.Transfer(100, &account002)
-
-	fmt.Println(status)
-	fmt.Println(account001)
-	fmt.Println(account002)
-}
-
-func (c *CheckingAccount) Withdraw(withdrawValue float64) string {
-	enabled := withdrawValue <= c.balance && withdrawValue > 0
-
-	if enabled {
-		c.balance -= withdrawValue
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *CheckingAccount) Deposit(depositValue float64) (string, float64) {
-	if depositValue > 0 {
-		c.balance += depositValue
-		return "Depósito realizado com sucesso", c.balance
-	} else {
-		return "Depósito não realizado, contate a gerência para mais infomações", c.balance
-	}
-}
-
-func (c *CheckingAccount) Transfer(transferValue float64, destinationAccount *CheckingAccount) bool {
-	if transferValue < c.balance && transferValue > 0 {
-		c.balance -= transferValue
-		destinationAccount.Deposit(transferValue)
-		return true
-	} else {
-		return false
-	}
+	fmt.Println(account001.GetBalance())
 }
