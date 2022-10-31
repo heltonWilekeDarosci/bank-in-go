ad926c6af8375a18d5607e5f4797ef6e371c400d
helton <heltoloco@hotmail.com>
2022-10-31 20:08:40 -0300

    feat(bank): create interface to pay bills method

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
 	"fmt"
 )
 
+func PayBill(account verifyAccount, billValue float64) {
+	account.Withdraw(billValue)
+	fmt.Println("Seu boleto foi pago com sucesso")
+}
+
+type verifyAccount interface {
+	Withdraw(value float64) string
+}
+
 func main() {
-	account001 := accounts.CheckingAccount{}
+	account001 := accounts.SavingsAccount{}
 	account001.Deposit(100)
+	PayBill(&account001, 30)
 
 	fmt.Println(account001.GetBalance())
 }
