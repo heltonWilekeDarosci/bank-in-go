8657bd21014eb57ec739aa4246c883ce88513c75
helton <heltoloco@hotmail.com>
2022-10-31 15:19:43 -0300

    feat(bank): apply transfer func

--- main.go
+++ main.go
@@ -10,15 +10,16 @@ type CheckingAccount struct {
 }
 
 func main() {
-	account001 := CheckingAccount{}
-	account001.holder = "Helton"
-	account001.agencyNumber = 123
-	account001.accountNumber = 123456
-	account001.balance = 500
+	account001 := CheckingAccount{holder: "Helton", balance: 500}
+	account002 := CheckingAccount{holder: "Mark", balance: 200}
 
-	fmt.Println(account001.balance)
-	fmt.Println(account001.Deposit(100))
-	fmt.Println(account001.balance)
+	fmt.Println(account001)
+	fmt.Println(account002)
+	status := account001.Transfer(100, &account002)
+
+	fmt.Println(status)
+	fmt.Println(account001)
+	fmt.Println(account002)
 }
 
 func (c *CheckingAccount) Withdraw(withdrawValue float64) string {
@@ -38,5 +39,15 @@ func (c *CheckingAccount) Deposit(depositValue float64) (string, float64) {
 		return "Depósito realizado com sucesso", c.balance
 	} else {
 		return "Depósito não realizado, contate a gerência para mais infomações", c.balance
+	}
+}
+
+func (c *CheckingAccount) Transfer(transferValue float64, destinationAccount *CheckingAccount) bool {
+	if transferValue < c.balance && transferValue > 0 {
+		c.balance -= transferValue
+		destinationAccount.Deposit(transferValue)
+		return true
+	} else {
+		return false
 	}
 }
